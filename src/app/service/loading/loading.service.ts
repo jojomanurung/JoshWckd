7e190273cfd37bf3082e776b233517885a7a4ed9
Joshua <joshuamlvn@gmail.com>
2021-10-23 22:53:46 +0700

    Create first shared component loading

---
+++ src/app/service/loading/loading.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { concatMap, finalize, tap } from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class LoadingService {
+	private loadingSubject = new BehaviorSubject<boolean>(false);
+
+	loading$: Observable<boolean> = this.loadingSubject.asObservable();
+
+	constructor() {}
+
+	loadingOn() {
+		this.loadingSubject.next(true);
+	}
+
+	loadingOff() {
+		this.loadingSubject.next(false);
+	}
+}
---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoadingService } from 'src/app/service/loading/loading.service';
+
+@Component({
+	selector: 'loading',
+	templateUrl: './loading.component.html',
+	styleUrls: ['./loading.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingComponent implements OnInit {
+	constructor(public loadingService: LoadingService) {}
+
+	ngOnInit(): void {}
+}
