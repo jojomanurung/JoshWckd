06dd1d38ef43569c7e6b862639b761b3169b4ecd
Joshua <joshuamlvn@gmail.com>
2021-11-25 18:33:20 +0700

    Update import module

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { SharedModule } from '../shared/shared.module';
 import { KanbanRoutingModule } from './kanban-routing.module';
 import { KanbanComponent } from './page/kanban.component';
 import { TaskComponent } from './components/task/task.component';
 import { TaskDialogComponent } from './components/task-dialog/task-dialog.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [KanbanComponent, TaskComponent, TaskDialogComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    KanbanRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, SharedModule, KanbanRoutingModule],
 })
 export class KanbanModule {}
