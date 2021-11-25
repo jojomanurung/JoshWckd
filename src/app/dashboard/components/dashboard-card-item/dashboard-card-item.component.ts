f3c7285dbc20344563eb6ca9f8407f2d274e60f2
Joshua <joshuamlvn@gmail.com>
2021-11-25 18:40:12 +0700

    Dynamic implementation

--- src/app/dashboard/components/dashboard-card-item/dashboard-card-item.component.ts
+++ src/app/dashboard/components/dashboard-card-item/dashboard-card-item.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-dashboard-card-item',
-	templateUrl: './dashboard-card-item.component.html',
-	styleUrls: ['./dashboard-card-item.component.scss'],
+  selector: 'app-dashboard-card-item',
+  templateUrl: './dashboard-card-item.component.html',
+  styleUrls: ['./dashboard-card-item.component.scss'],
 })
 export class DashboardCardItemComponent implements OnInit {
+  @Input() project: any;
   image = 'src/assets/kanban.PNG';
-	constructor() {}
+  constructor(private router: Router) {}
 
-	ngOnInit(): void {}
+  ngOnInit(): void {}
+
+  openProject(link: any) {
+    if (link.includes('/')) {
+      link = link.replace('/', '');
+    }
+    this.router.navigate([link]);
+  }
 }
