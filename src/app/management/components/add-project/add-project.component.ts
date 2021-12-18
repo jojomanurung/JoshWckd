b3716f12a26fe1ffe9b9f0a16d6d4b5bb480dae6
Joshua <joshuamlvn@gmail.com>
2021-12-19 02:06:12 +0700

    Create upload image ability to management

--- src/app/management/components/add-project/add-project.component.ts
+++ src/app/management/components/add-project/add-project.component.ts
@@ -3,31 +3,42 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
 } from '@angular/core';
+import { AngularFireUploadTask } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { ManagementService } from 'src/app/service/management/management.service';
+import { UploadService } from 'src/app/service/upload/upload.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.scss'],
 })
-export class AddProjectComponent implements OnInit, OnChanges {
+export class AddProjectComponent implements OnInit, OnChanges, OnDestroy {
   @Input() project: any;
   @Output() action = new EventEmitter();
+  private subs = new SubSink();
   form!: FormGroup;
   initialForm: any;
   editMode = false;
+  percentage!: Observable<any>;
+  snapshot!: Observable<any>;
+  isUploading!: boolean;
 
   constructor(
     private fb: FormBuilder,
-    private managementService: ManagementService
+    private managementService: ManagementService,
+    private uploadService: UploadService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     this.initForm();
@@ -56,6 +67,11 @@ export class AddProjectComponent implements OnInit, OnChanges {
       description: ['', Validators.required],
       link: ['', Validators.required],
       active: [false],
+      screenShot: this.fb.group({
+        name: [''],
+        safeName: [''],
+        link: [''],
+      }),
     });
     this.initialForm = this.form.getRawValue();
   }
@@ -77,6 +93,52 @@ export class AddProjectComponent implements OnInit, OnChanges {
     }
   }
 
+  handleUpload(event: any) {
+    const file: File = event.target.files[0];
+    const acceptable = ['jpg', 'jpeg', 'png', 'svg'];
+    // console.log(file);
+
+    if (file) {
+      const fileType = this.uploadService
+        .getFileType(file.name)
+        ?.toLocaleLowerCase();
+
+      if (acceptable.includes(fileType!)) {
+        this.isUploading = true;
+
+        const uniqName = this.uploadService.getUniqueSafeName(file);
+
+        this.form.get('screenShot')?.get('name')?.patchValue(file.name);
+
+        this.form
+          .get('screenShot')
+          ?.get('safeName')
+          ?.patchValue(uniqName.uniqueSafeName);
+
+        const uploadTask = this.uploadService.uploadFile(uniqName.path, file);
+        this.percentage = uploadTask.percentageChanges();
+        this.snapshot = uploadTask.snapshotChanges().pipe(
+          finalize(async () => {
+            const downloadUrl = await uniqName.ref.getDownloadURL().toPromise();
+            const fileUrl = downloadUrl;
+            this.form.get('screenShot')?.get('link')?.patchValue(fileUrl);
+            // console.log(fileUrl);
+            this.isUploading = false;
+          })
+        );
+        this.subs.sink = this.snapshot.subscribe();
+      }
+    }
+  }
+
+  async deleteImg() {
+    const downloadUrl = this.form.get('screenShot')?.get('link')?.value;
+    const deletes = await this.uploadService.deleteFile(downloadUrl);
+    this.form.get('screenShot')?.get('name')?.patchValue('');
+    this.form.get('screenShot')?.get('safeName')?.patchValue('');
+    this.form.get('screenShot')?.get('link')?.patchValue('');
+  }
+
   save() {
     if (!this.editMode) {
       const payload = this.form.value;
@@ -90,5 +152,9 @@ export class AddProjectComponent implements OnInit, OnChanges {
       this.managementService.editProject(payload);
       this.action.emit('save');
     }
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
