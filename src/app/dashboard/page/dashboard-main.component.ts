1cf300b9630803fc941d77b8f531a09e64432d46
Joshua <joshuamlvn@gmail.com>
2022-05-03 14:36:40 +0700

    Add ngx-scrollbar for smooth scroll

--- src/app/dashboard/page/dashboard-main.component.ts
+++ src/app/dashboard/page/dashboard-main.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { LoadingService } from 'src/app/service/loading/loading.service';
 import { ManagementService } from 'src/app/service/management/management.service';
 import { SubSink } from 'subsink';
 import * as _ from 'lodash-es';
 import { NavService } from 'src/app/service/nav/nav.service';
+import { NgScrollbar } from 'ngx-scrollbar';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard-main',
   templateUrl: './dashboard-main.component.html',
   styleUrls: ['./dashboard-main.component.scss'],
 })
-export class DashboardMainComponent implements OnInit, OnDestroy {
+export class DashboardMainComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(NgScrollbar) scrollbarRef!: NgScrollbar;
   private subs = new SubSink();
   isMobile = false;
   project!: any[];
@@ -24,6 +27,13 @@ export class DashboardMainComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.layoutObserver();
     this.getProject();
+  }
+
+  ngAfterViewInit(): void {
+    this.subs.sink = this.scrollbarRef.verticalScrolled
+    .subscribe((resp) => {
+      console.log(resp);
+    })
   }
 
   layoutObserver() {
@@ -42,6 +52,14 @@ export class DashboardMainComponent implements OnInit, OnDestroy {
         this.isLoading = false;
       }
     );
+  }
+
+  smoothScroll(elem: string) {
+    if (elem === '#home') {
+      this.scrollbarRef.scrollTo({top: 0, duration: 2500, easing: {x1: .4, y1: .75, x2: .75, y2: .75}});
+      return;
+    }
+    this.scrollbarRef.scrollToElement(elem, {duration: 2500, easing: {x1: .4, y1: .75, x2: .75, y2: .75}});
   }
 
   ngOnDestroy(): void {
