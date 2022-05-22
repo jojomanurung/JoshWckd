c80543583585def305e3bfa299db48b257a2ee0b
Joshua <joshuamlvn@gmail.com>
2022-05-22 23:20:26 +0700

    Add grid system to navbar, group menu links, animation menu links

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,21 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, HostBinding, Input } from '@angular/core';
 import { MenuItem } from '@shared/interface/nav-item/nav-item';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('showChild', [
+      transition(':leave', [
+        style({ transform: 'translateY(0)', opacity: 1 }),
+        animate(
+          '225ms cubic-bezier(0.4,0.0,0.2,1)',
+          style({ transform: 'translateY(-10px)', opacity: 0 })
+        ),
+      ]),
+      transition(':enter', [
+        style({ transform: 'translateY(-10px)', opacity: 0 }),
+        animate(
+          '225ms cubic-bezier(0.4,0.0,0.2,1)',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements AfterViewInit {
+  @HostBinding('@showChild') animate = false;
   @Input() isMobile!: boolean;
   menuItems = MenuItem;
   logo = '../../assets/images/logo.png';
   version = environment.appVersion;
+  expanded = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.animate = true;
   }
-
 }
