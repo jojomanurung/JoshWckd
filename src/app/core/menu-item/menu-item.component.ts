9fbda8235e28fb3f507ad2cdf2d2f553899db73c
Joshua <joshuamlvn@gmail.com>
2022-05-26 00:48:06 +0700

    Animate menu item when collapsing navbar

--- src/app/core/menu-item/menu-item.component.ts
+++ src/app/core/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   animate,
@@ -9,6 +9,7 @@ import {
 } from '@angular/animations';
 import { MenuItem } from '@shared/interface/menu-items/menu-items';
 import { NavService } from '@service/nav/nav.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-menu-item',
@@ -39,27 +40,49 @@ import { NavService } from '@service/nav/nav.service';
         ),
       ]),
     ]),
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-5px)', opacity: 0 }),
+        animate(
+          '225ms cubic-bezier(0.4,0.0,0.2,1)',
+          style({ transform: 'translateX(0)', opacity: 1 })
+        ),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate(
+          '225ms cubic-bezier(0.4,0.0,0.2,1)',
+          style({ transform: 'translateX(-5px)', opacity: 0 })
+        ),
+      ]),
+    ]),
   ],
 })
-export class MenuItemComponent implements OnInit {
+export class MenuItemComponent implements OnInit, OnDestroy {
   expanded = false;
   isMobile = false;
   @HostBinding('attr.aria-expanded') ariaExpanded = this.expanded;
   @Input() item!: MenuItem;
   @Input() depth!: number;
+  @Input() isNavCollapsed!: boolean;
+  private subs = new SubSink();
 
-  constructor(public navService: NavService, public router: Router) {
+  constructor(public navService: NavService, public router: Router) {}
+
+  ngOnInit() {
     if (this.depth === undefined) {
       this.depth = 0;
     }
 
-    this.navService.isMobile.subscribe(
+    this.subs.sink = this.navService.isMobile.subscribe(
       (isMobile) => (this.isMobile = isMobile)
     );
+
+    this.observeCurrentUrl();
   }
 
-  ngOnInit() {
-    this.navService.currentUrl.subscribe((url: string) => {
+  observeCurrentUrl() {
+    this.subs.sink = this.navService.currentUrl.subscribe((url: string) => {
       if (this.item.route && url && url.indexOf(`/${this.item.route}`) === 0) {
         this.expanded = url.indexOf(`/${this.item.route}`) === 0;
         this.ariaExpanded = this.expanded;
@@ -85,5 +108,10 @@ export class MenuItemComponent implements OnInit {
         }
       }
     });
+  }
+
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
