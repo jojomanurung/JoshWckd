1725458d8306b5145efaa79ced22cce427bec153
Joshua <joshuamlvn@gmail.com>
2022-07-20 01:05:07 +0700

    Create sign up page

--- src/app/sessions/components/signup/signup.component.ts
+++ src/app/sessions/components/signup/signup.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@service/auth/auth.service';
+import { LoadingService } from '@service/loading/loading.service';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService, private loadingService: LoadingService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  async signUp() {
+    if (this.signForm.invalid) {
+      this.signForm.markAllAsTouched();
+      return;
+    }
+    this.loadingService.loadingOn();
+    const email = this.signForm.get('email')?.value;
+    const password = this.signForm.get('password')?.value;
+    await this.authService.emailSignUp(email, password);
+    this.loadingService.loadingOff();
+    this.router.navigate(['/session/sign-in']);
+  }
+
+  async googleSignIn() {
+    this.loadingService.loadingOn();
+    await this.authService.googleSignIn();
+    this.loadingService.loadingOff();
   }
 
 }
